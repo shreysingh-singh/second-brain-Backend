f0ca1a0e3f3cb5b408cbc6382da7754c89917a41
Shrey <shreysingh11733@gmail.com>
2026-04-06 13:31:48 +0530

    Updated PORT

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json({ strict: false }));
 app.use(
   cors({
     origin: [
-      "http://localhost:5174",
+      "http://localhost:5173",
       "http://localhost:5175",
       "http://localhost:5176",
       "http://localhost:3000",
